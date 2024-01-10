a72920f4372252bf9956c4fa193b68461b1f8256
Abhay Singh <93770368+Abhay-Singh-1913@users.noreply.github.com>
2024-01-10 11:32:50 +0530

    Update Controller.java

--- src/main/java/com/jsp/urs/controller/Controller.java
+++ src/main/java/com/jsp/urs/controller/Controller.java
@@ -1,5 +1,6 @@
 package com.jsp.urs.controller;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import com.jsp.urs.model.User;
@@ -7,42 +8,51 @@ import com.jsp.urs.model.UserData;
 
 public class Controller {
 
-	private UserData userdata = new UserData();
-
 	public void addUser(User user) {
-		UserData.getUsers();
-		if (Users == null) {	
-			Users = new ArrayList<>();
-			userdata.setUsers(Users);
+		List<User> users = UserData.getUsers();
+		if (users == null) {	
+			users = new ArrayList<User>();
+			UserData.setUsers(users);
 		}
-		Users.add(user);
+		users.add(user);
 	}
-//
-//	public User searchUser(String username) {
-//		List<User> users = userdata.getUsers();
-//		Iterator<User> iterator = users.iterator();
-//		while (iterator.hasNext()) {
-//			User user = (User) iterator.next();
-//			if (user.getUsername().equalsIgnoreCase(username)) {
-//				return user;
-//			}
-//			
-//		}
-//		return null;
-//	}
-//
-//	public boolean updateUserPasswordByUserName(User refUser, String User_password) {
-//		User User = searchUser(refUser.getUsername());
-//		if (User != null) {
-//			User.setUsername(refUser.getUsername());
-//			User.setPassword(User_password);
-//			return true;
-//		}
-//		return false;
-//	}
-//
-//	public List<User> getAllUsers() {
-//		return userdata.getUsers();
-//	}
+
+	public User searchUser(String username, String password) {
+		List<User> users = UserData.getUsers();
+		Iterator<User> iterator = users.iterator();
+		while (iterator.hasNext()) {
+			User user = (User) iterator.next();
+			if (user.getUsername().equalsIgnoreCase(username) && user.getPassword().equals(password)) {
+				return user;
+			}
+		}
+		return null;
+	}
+	
+	public User searchUsername(String username) {
+		List<User> users = UserData.getUsers();
+		Iterator<User> iterator = users.iterator();
+		while (iterator.hasNext()) {
+			User user = (User) iterator.next();
+			if (user.getUsername().equalsIgnoreCase(username)) {
+				return user;
+			}
+		}
+		return null;
+	}
+	
+
+	public boolean updateUserPasswordByUserName(User refUser, String User_password) {
+		if (refUser != null) {
+			refUser.setUsername(refUser.getUsername());
+			refUser.setPassword(User_password);
+			return true;
+		}
+		return false;
+	}
+
+	public List<User> getAllUsers() {
+		return UserData.getUsers();
+	}
 
 }
